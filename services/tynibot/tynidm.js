c31765b9452aaa8f1ca58ba88bf5801a37a304b3
Erriccc <ozyjunks@gmail.com>
2024-01-11 05:30:28 -0600

    flowise200text

--- services/tynibot/tynidm.js
+++ services/tynibot/tynidm.js
@@ -82,11 +82,10 @@ const tynidm = async (req, res) => {
     }
 
     query({
-        "question": "Hey, how are you?"
-        // "question": "Hey, how are you?",
-        // "overrideConfig": {
-        //     "sessionId": id
-        // }
+        "question": "Hey, how are you?",
+        "overrideConfig": {
+            "sessionId": 'kelvinnnnnn'
+        }
     }).then((response) => {
         console.log(response);
         res.status(200).json({ yoo: 'yooooo', response });
